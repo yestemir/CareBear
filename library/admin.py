3c5aa354a767fd987142daa0536ede858bc8064c
Gaziza <yestemirova.g@chocofood.kz>
2021-03-12 11:52:36 +0600

    Add genre admin

--- library/admin.py
+++ library/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from library.models import Book, Review, RentedBook, SavedBook
+from library.models import Book, Review, RentedBook, SavedBook, Genre
 
 admin.site.register(Book)
 admin.site.register(RentedBook)
 admin.site.register(Review)
 admin.site.register(SavedBook)
+admin.site.register(Genre)
