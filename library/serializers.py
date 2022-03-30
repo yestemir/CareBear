0c4814e4c182c6c1424b8aba689067177cf4fe3b
Дина Естемир <dina.yestemir@gmail.com>
2022-03-30 20:04:46 +0600

    add everyday feature

--- library/serializers.py
+++ library/serializers.py
@@ -1,19 +1,20 @@
 from django.contrib.auth.models import User
+from django.forms import model_to_dict
 from rest_framework import serializers
 
 from library.models import HealthStatus, Checkbox
 
 
-# class CheckboxesSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Checkboxes
-#         fields = ('id', 'title', 'checkboxes')
-
-
 class CheckboxSerializer(serializers.ModelSerializer):
     class Meta:
         model = Checkbox
-        fields = ('id', 'name', 'title', 'done', 'everyday', 'type', 'health_status', 'user_id')
+        fields = ('id', 'name', 'title', 'done', 'everyday', 'type', 'health_status', 'user_id',)
+        # extra_kwargs = {
+        #     "id": {
+        #         "read_only": False,
+        #         "required": False,
+        #     },
+        # }
 
 
 class HealthCheckSerializer(serializers.ModelSerializer):
@@ -21,6 +22,7 @@ class HealthCheckSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         checkbox_data = validated_data.pop('checkbox')
+        checkbox_data.extend(self.get_last_active_checkbox_data(validated_data.get('user')))
         health_status = HealthStatus.objects.create(**validated_data)
         health_status.save()
         for data in checkbox_data:
@@ -38,8 +40,21 @@ class HealthCheckSerializer(serializers.ModelSerializer):
         for data in checkbox_data:
             data['health_status'] = instance
             data['user_id'] = instance.user
+            # if data.get('id') is not None:
+            #     Checkbox.objects.filter(pk=data.pop('id')).update(**data)
+            # else:
             Checkbox.objects.create(**data)
         return instance
+
+    def get_last_active_checkbox_data(self, user_id: int):
+        health_status = HealthStatus.objects.filter(user_id=user_id).order_by('date').last()
+        checkbox_data = []
+        for data in health_status.checkbox.all():
+            if data.everyday is True:
+                old_checkbox_data = model_to_dict(data)
+                old_checkbox_data.pop('id')
+                checkbox_data.append(old_checkbox_data)
+        return checkbox_data
 
     class Meta:
         model = HealthStatus
--- library/views.py
+++ library/views.py
@@ -72,55 +72,6 @@ class CheckboxViewSet(ModelViewSet):
         return Response(serializer.data)
 
 
-# class CheckboxesViewSet(ModelViewSet):
-#     queryset: QuerySet = Checkboxes.objects.all()
-#     serializer_class = CheckboxesSerializer
-#     http_method_names = ('get', 'post')
-#     filter_backends: Tuple = (SearchFilter, DjangoFilterBackend)
-#     # search_fields: Tuple = ('text',)
-#     # filterset_fields: Tuple = ('book_id',)
-#     permission_classes = (IsAuthenticated,)
-#
-#     def list(self, request, *args, **kwargs):
-#         queryset = self.get_queryset().filter(
-#             user_id=request.user.pk
-#         )
-#         page = self.paginate_queryset(queryset)
-#         if page is not None:
-#             serializer = self.get_serializer(page, many=True)
-#             return self.get_paginated_response(serializer.data)
-#
-#         serializer = self.get_serializer(queryset, many=True)
-#         return Response(serializer.data)
-
-
-# class UserHealthStatusViewSet(ModelViewSet):
-#     queryset: QuerySet = UserHealthStatus.objects.all()
-#     serializer_class = UserHealthStatus
-#     http_method_names = ('get', 'post', 'delete')
-#     permission_classes = (IsAuthenticated,)
-#
-#     def list(self, request, *args, **kwargs):
-#         queryset = self.get_queryset().filter(
-#             user_id=request.user.pk
-#         )
-#         page = self.paginate_queryset(queryset)
-#         if page is not None:
-#             serializer = self.get_serializer(page, many=True)
-#             return self.get_paginated_response(serializer.data)
-#
-#         serializer = self.get_serializer(queryset, many=True)
-#         return Response(serializer.data)
-
-
-# class GenreViewSet(ModelViewSet):
-#     queryset: QuerySet = Genre.objects.all()
-#     serializer_class = GenreSerializer
-#     http_method_names = ('get',)
-#     filter_backends: Tuple = (SearchFilter,)
-#     search_fields: Tuple = ('title',)
-
-
 class UserViewSet(ModelViewSet):
     queryset: QuerySet = User.objects.all()
     serializer_class = UserSerializer
