c04afaff0b8d8921105cbfb6c41e6f3f6b7e9237
Дина Естемир <dina.yestemir@gmail.com>
2022-04-18 23:20:39 +0600

    add to lib admin

--- library/admin.py
+++ library/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from library.models import HealthStatus, Checkbox
+from library.models import HealthStatus, Checkbox, Post, Comment
 
 admin.site.register(HealthStatus)
 admin.site.register(Checkbox)
+admin.site.register(Post)
+admin.site.register(Comment)
