5ec458a356a791aaa305ac86577960cc8626ccae
Дина Естемир <dina.yestemir@gmail.com>
2022-04-27 01:13:28 +0600

    fix pagination

--- library/paginations.py
+++ library/paginations.py
@@ -5,4 +5,4 @@ class CustomPagination(pagination.PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 50
-    page_query_param = 'p'
+    page_query_param = 'page'
