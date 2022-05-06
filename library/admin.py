44e85360b1a6d910ff3b485aa8f7f1f9f30fb3a7
Dina Yestemir <dina.yestemir@gmail.com>
2022-05-06 17:05:45 +0600

    Add models to admin page

--- library/admin.py
+++ library/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from library.models import HealthStatus, Checkbox, Post, Comment
+from library.models import HealthStatus, Checkbox, Post, Comment, Badge, UserBadge
 
 admin.site.register(HealthStatus)
 admin.site.register(Checkbox)
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(Badge)
+admin.site.register(UserBadge)
